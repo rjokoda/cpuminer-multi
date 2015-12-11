e8b46cd46854a31114f128baac0d56f441005579
elbandi <bandispam@gmail.com>
2015-12-11 13:57:39 +0100

    Changes for automated build

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
+bs_dir="$(dirname $(readlink -f $0))"
 
-# You need autoconf 2.5x, preferably 2.57 or later
-# You need automake 1.7 or later. 1.6 might work.
-
-set -e
-
-aclocal
-autoheader
-automake --foreign --add-missing --force-missing --copy
-# automake --gnu --add-missing --copy
-autoconf
-
+#Some versions of libtoolize don't like there being no ltmain.sh file already
+touch "${bs_dir}"/ltmain.sh
+autoreconf -fi "${bs_dir}"
